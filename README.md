4b60228079f0f0c384cbed22f5414138cf199702
Christoph Dorn <christoph@christophdorn.com>
2017-01-26 00:06:03 -0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bash.origin.express
+
+[![CircleCI](https://circleci.com/gh/bash-origin/bash.origin.express.svg?style=svg)](https://circleci.com/gh/bash-origin/bash.origin.express)
--- examples/01-HelloWorld/main.sh
+++ examples/01-HelloWorld/main.sh
@@ -1,17 +1,39 @@
 #!/usr/bin/env bash.origin.script
 
+export PORT="8080"
+
 depend {
     "pages": "@com.github/bash-origin/bash.origin.express#1"
 }
 
 function PRIVATE_Run {
 
+    return;
+
+# TODO: Use 'CALL_start' which uses bash.origin.process (mon) to start server
+#       so we can then make request usign curl and then stop server again.
     CALL_pages run {
         "routes": {
             "/": "Hello World!"
         }
     }
+}
 
+function PRIVATE_Verify {
+    # TODO: Instead of just sleeping for 1 second, use curl to call server
+    #       until we get a reponse or timeout. Use 'bash.origin.request' to
+    #       make the calls.
+    sleep 1
+    local requestID=`uuidgen`
+    local command="curl -s "http://${DOCKER_CONTAINER_HOST_IP}:${PORT}/?rid=$requestID""
+    echo "Command: $command"
+    local response=`$command`
+    echo "Response: $response"
+
+    #if [ "${response}" != "Hello World from dockerized NodeJS process!" ]; then
+    #		echo "ERROR: Did not get expected response!"
+    #		exit 1
+    #fi
 }
 
 PRIVATE_Run "$@"
